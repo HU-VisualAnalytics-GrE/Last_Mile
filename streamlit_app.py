fd0af49a16467da920ca909c3c2c97ea5c903c1c
Bauer, Benjamin <benjaminmoritz.bauer@gmail.com>
2025-02-11 10:39:41 +0100

    Add autoencoder and option to train new model

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 # -------------- Imports --------------
+import os
 import pickle
 
 import pandas as pd
@@ -8,6 +9,7 @@ import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
 import streamlit as st
+st.set_page_config(layout="wide")
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
@@ -102,7 +104,7 @@ def find_interval(value, interval_info):
         Information about the matching interval including interval number and boundaries
     """
     for boundary in interval_info['boundaries']:
-        if boundary['start_col'] <= value <= boundary['end_col']:
+        if float(boundary['start_col']) <= float(value) <= float(boundary['end_col']):
             return {
                 'interval_number': boundary['interval'],
                 'start': boundary['start_col'],
@@ -238,7 +240,7 @@ def analyze_misclassifications(y_test, predictions):
     return cm, cmn
 
 
-def confussion_matrix(cm, unique_key):
+def confussion_matrix(cm, unique_key, x, y, pred):
     class_names = ['very high', 'high', 'moderate', 'low', 'very low']
     fig = px.imshow(cm, text_auto='.2f', color_continuous_scale="blues")
     fig.update_layout(
@@ -262,10 +264,10 @@ def confussion_matrix(cm, unique_key):
         point = selected_points[0]
         true_label = get_label_from_index(int(point['x']))
         pred_label = get_label_from_index(int(point['y']))
-        show_pca_for_labels(true_label, pred_label, unique_key)
+        show_pca_for_labels(true_label, pred_label, unique_key, x, y, pred)
 
 
-def confussion_matrix_normalized(cmn, unique_key):
+def confussion_matrix_normalized(cmn, unique_key, x, y, pred):
     class_names = ['very high', 'high', 'medium', 'low', 'very low']
     fig = px.imshow(cmn, text_auto='.2f', color_continuous_scale="blues")
     fig.update_layout(
@@ -302,7 +304,7 @@ def confussion_matrix_normalized(cmn, unique_key):
         point = selected_points[0]
         true_label = get_label_from_index(int(point['x']))
         pred_label = get_label_from_index(int(point['y']))
-        show_pca_for_labels(true_label, pred_label, unique_key)
+        show_pca_for_labels(true_label, pred_label, unique_key, x, y, pred)
 
 
 # -------------- PCA Functions --------------
@@ -372,8 +374,8 @@ def scatter_plot_df(df, true_label, predicted_label):
     return fig
 
 
-def show_pca_for_labels(true_label, pred_label, unique_key):
-    pca_df = prepare_pca_data(X_test, y_test, predictions)
+def show_pca_for_labels(true_label, pred_label, unique_key, x, y, pred):
+    pca_df = prepare_pca_data(x, y, pred)
     filtered_df = adjust_df_for_plot(pca_df, true_label, pred_label)
     fig = scatter_plot_df(filtered_df, true_label, pred_label)
     st.plotly_chart(fig, use_container_width=True, key=f"pca_{unique_key}")
@@ -386,7 +388,7 @@ def show_pca_for_labels(true_label, pred_label, unique_key):
     }))
 
 
-def train_or_load_model(path, x_train=None, y_train=None):
+def train_or_load_model(path, x_train=None, y_train=None, n_estimators=100, max_depth=None):
     """
     Used to either train or load the model. If a valid path is provided, the model will be loaded from that path.
     If there is no model existing at the path, it will be trained.
@@ -407,7 +409,7 @@ def train_or_load_model(path, x_train=None, y_train=None):
         # Load and prepare data
         # Train model and make predictions
         if path.endswith(".pkl"):
-            model = train_rf_model(x_train, y_train)
+            model = train_rf_model(x_train, y_train, n_estimators, max_depth)
             with open(path, 'wb') as f:
                 pickle.dump(model, f)
             return model
@@ -419,12 +421,38 @@ if __name__ == "__main__":
     df_target, df_test = load_data()
 
     # Setup Streamlit interface
+
     st.sidebar.title("Options")
     option_cmn = st.sidebar.checkbox("Normalize Confusion Matrix", value=True)
-
+    model_dir = "models"
+    model_names = [f for f in os.listdir(model_dir) if os.path.isfile(os.path.join(model_dir, f)) and f.endswith(".pkl")]
     st.sidebar.header("Select Models for Comparison")
-    option_model_selection_1 = st.sidebar.selectbox("Select Model 1", ["Unoptimized Model", "Optimized Model"])
-    option_model_selection_2 = st.sidebar.selectbox("Select Model 2", ["Optimized Model", "Unoptimized Model"])
+    option_model_selection_1 = st.sidebar.selectbox("Select Model 1", model_names, index=model_names.index("RandomForestClassifierUnoptimized.pkl"))
+    option_model_selection_2 = st.sidebar.selectbox("Select Model 2", model_names, index=model_names.index("RandomForestClassifierOptimized.pkl"))
+    new_model = st.sidebar.popover("Train new Model")
+    with new_model:
+        st.write("### Set Model Parameters")
+
+        model_name = st.text_input("Model Name", value="New_Model.pkl")
+        num_estimators = st.number_input("Num Estimators", value=100)
+        max_depth = st.number_input("Max Depth", value=10)
+        optimized = st.checkbox("Optimize using pretrained Auto Encoder", value=False)
+        if st.button("Start Training"):
+            st.success(
+                f"Training started for model: {model_name} with {num_estimators} Estimators at {max_depth} max depth.")
+            print(optimized)
+            if not optimized:
+                X_train_temp, X_test_temp, y_train_temp, y_test_temp = prepare_data(df_test, df_target)
+                rf_model = train_or_load_model(f"{model_dir}/{model_name}", X_train_temp, y_train_temp, num_estimators,
+                                             max_depth)
+            else:
+                encoder = train_or_load_model(f"{model_dir}/autoencoder_model.h5")
+                encoded_features = encoder.predict(df_test)
+                df_test_temp = pd.DataFrame(encoded_features)
+                X_train_temp, X_test_temp, y_train_temp, y_test_temp = prepare_data(df_test_temp, df_target)
+                rf_model = train_or_load_model(f"{model_dir}/{model_name}", X_train_temp,
+                                                 y_train_temp, num_estimators, max_depth)
+            st.success(f"Training done. Refresh the page.")
 
     st.sidebar.header("Variables for Feature Importance Analysis")
     option_number_top_features = st.sidebar.slider("Top Features to display", min_value=1, max_value=50, value=5)
@@ -433,77 +461,80 @@ if __name__ == "__main__":
     st.title("Lucas Organic Carbon Dataset")
     st.header("Overview of Misclassifications")
 
-    misclassification_model_1, misclassification_model_2 = st.columns(2, gap="small")
+    misclassification_model_1, misclassification_model_2 = st.columns(2, gap="medium")
 
     with misclassification_model_1:
         
         st.subheader(option_model_selection_1)
-        if option_model_selection_1 == "Unoptimized Model":
-            X_train, X_test, y_train, y_test = prepare_data(df_test, df_target)
-            rf_model_1 = train_or_load_model("models/RandomForestClassifierUnoptimized.pkl", X_train, y_train)
-        else:
-            encoder = train_or_load_model("models/autoencoder_model.h5")
+        if "Unoptimized" in option_model_selection_1:
+            df_test_1 = df_test
+            X_train_1, X_test_1, y_train_1, y_test_1 = prepare_data(df_test_1, df_target)
+            rf_model_1 = train_or_load_model(f"{model_dir}/{option_model_selection_1}", X_train_1, y_train_1, 250, 16)
+        elif "Optimized" in option_model_selection_1:
+            encoder = train_or_load_model(f"{model_dir}/autoencoder_model.h5")
             encoded_features = encoder.predict(df_test)
-            df_encoded_features = pd.DataFrame(encoded_features)
-            X_train, X_test, y_train, y_test = prepare_data(df_encoded_features, df_target)
-            rf_model_1 = train_or_load_model("models/RandomForestClassifierOptimized.pkl", X_train, y_train)
+            df_test_1 = pd.DataFrame(encoded_features)
+            X_train_1, X_test_1, y_train_1, y_test_1 = prepare_data(df_test_1, df_target)
+            rf_model_1 = train_or_load_model(f"{model_dir}/RandomForestClassifierOptimized.pkl", X_train_1, y_train_1)
 
-        predictions = rf_model_1.predict(X_test)
+        predictions_1 = rf_model_1.predict(X_test_1)
+        st.write(f"Accuracy: {round(accuracy_score(y_test_1, predictions_1), 4)}")
 
         # Create confusion matrix
-        cm, cmn = analyze_misclassifications(y_test, predictions)
+        cm_1, cmn_1 = analyze_misclassifications(y_test_1, predictions_1)
 
         # Display appropriate matrix
         if option_cmn:
-            confussion_matrix_normalized(cmn, 1)
+            confussion_matrix_normalized(cmn_1, 1, X_test_1, y_test_1, predictions_1)
         else:
-            confussion_matrix(cm, 1)
+            confussion_matrix(cm_1, 1, X_test_1, y_test_1, predictions_1)
 
     with misclassification_model_2:
 
         st.subheader(option_model_selection_2)
-        if option_model_selection_2 == "Optimized Model":
-            encoder = train_or_load_model("models/autoencoder_model.h5")
+        if "Unoptimized" in option_model_selection_2:
+            df_test_2 = df_test
+            X_train_2, X_test_2, y_train_2, y_test_2 = prepare_data(df_test_2, df_target)
+            rf_model_2 = train_or_load_model(f"{model_dir}/RandomForestClassifierUnoptimized.pkl", X_train_2, y_train_2)
+        elif "Optimized" in option_model_selection_2:
+            encoder = train_or_load_model(f"{model_dir}/autoencoder_model.h5")
             encoded_features = encoder.predict(df_test)
-            df_encoded_features = pd.DataFrame(encoded_features)
-            X_train, X_test, y_train, y_test = prepare_data(df_encoded_features, df_target)
-            rf_model_2 = train_or_load_model("models/RandomForestClassifierOptimized.pkl", X_train, y_train)
-        else:
-            X_train, X_test, y_train, y_test = prepare_data(df_test, df_target)
-            rf_model_2 = train_or_load_model("models/RandomForestClassifierUnoptimized.pkl", X_train, y_train)
+            df_test_2 = pd.DataFrame(encoded_features)
+            X_train_2, X_test_2, y_train_2, y_test_2 = prepare_data(df_test_2, df_target)
+            rf_model_2 = train_or_load_model(f"{model_dir}/RandomForestClassifierOptimized_250.pkl", X_train_2, y_train_2)
 
-        predictions = rf_model_2.predict(X_test)
-        print(f"Optimized Accuracy: {accuracy_score(y_test, predictions)}")
+        predictions_2 = rf_model_2.predict(X_test_2)
+        st.write(f"Accuracy: {round(accuracy_score(y_test_2, predictions_2), 4)}")
         # Create confusion matrix
-        cm, cmn = analyze_misclassifications(y_test, predictions)
+        cm_2, cmn_2 = analyze_misclassifications(y_test_2, predictions_2)
 
         # Display appropriate matrix
         if option_cmn:
-            confussion_matrix_normalized(cmn, 2)
+            confussion_matrix_normalized(cmn_2, 2, X_test_2, y_test_2, predictions_2)
         else:
-            confussion_matrix(cm, 2)
+            confussion_matrix(cm_2, 2, X_test_2, y_test_2, predictions_2)
 
     st.header("Feature Importances")
     top_n = option_number_top_features
     feature_importance_1 = rf_model_1.feature_importances_
     feature_importance_2 = rf_model_2.feature_importances_
 
-    feature_importance_model_1, feature_importance_model_2 = st.columns(2, gap="small")
+    feature_importance_model_1, feature_importance_model_2 = st.columns(2, gap="medium")
 
     with feature_importance_model_1:
         st.subheader(option_model_selection_1)
 
         sorted_idx = np.argsort(feature_importance_1)[::-1][:top_n]  # Korrekte Sortierung
         sorted_importance = feature_importance_1[sorted_idx]
-        sorted_features = df_test.columns[sorted_idx]
+        sorted_features = df_test_1.columns[sorted_idx]
 
-        top_features_df = pd.DataFrame({
+        top_features_df_1 = pd.DataFrame({
             'Feature': sorted_features,
             'Importance': sorted_importance
         })
 
         fig = px.bar(
-            top_features_df,
+            top_features_df_1,
             x='Feature',
             y='Importance',
             orientation='v',
@@ -515,38 +546,38 @@ if __name__ == "__main__":
 
         selected_feature = st.selectbox(
             "Select a feature:",
-            options=top_features_df['Feature'],
+            options=top_features_df_1['Feature'],
             key="feature_selection_1"
         )
 
         if selected_feature:
             num_intervals = option_number_intervals
-            intervals, interval_info = partition_dataframe(X_test, num_intervals)
-            selected_interval = find_interval(selected_feature, interval_info)
+            intervals_1, interval_info_1 = partition_dataframe(X_test_1, num_intervals)
+            selected_interval_1 = find_interval(selected_feature, interval_info_1)
 
             # Ergebnisse anzeigen
-            st.write(f"Selected interval: **{selected_interval['interval_number']}** (of {num_intervals})")
-            st.write(f"Start value: **{selected_interval['start']}**")
-            st.write(f"End value: **{selected_interval['end']}**")
-            st.write(f"Number of features in interval: **{selected_interval['n_columns']}**")
+            st.write(f"Selected interval: **{selected_interval_1['interval_number']}** (of {num_intervals})")
+            st.write(f"Start value: **{selected_interval_1['start']}**")
+            st.write(f"End value: **{selected_interval_1['end']}**")
+            st.write(f"Number of features in interval: **{selected_interval_1['n_columns']}**")
 
-        selected_interesting_interval = st.selectbox(
+        selected_interesting_interval_1 = st.selectbox(
             "Select an interval:",
             options=[f"{i}" for i in range(1, num_intervals + 1)],
             key="interval_selection_1"
         )
 
-        if selected_interesting_interval:
-            results = calculate_interval_error_rf(
-                df_test,
-                int(selected_interesting_interval) - 1,
-                y_test,
-                intervals,
-                interval_info,
+        if selected_interesting_interval_1:
+            results_1 = calculate_interval_error_rf(
+                df_test_1,
+                int(selected_interesting_interval_1) - 1,
+                y_test_1,
+                intervals_1,
+                interval_info_1,
                 rf_model_1
             )
 
-            st.write(f"Accuracy Results before adding left and right boundary points: **{results:.4f}**")
+            st.write(f"Accuracy Results before adding left and right boundary points: **{results_1:.4f}**")
 
             #combined_points = combine_interval_points(
                 #df_test,
@@ -569,22 +600,22 @@ if __name__ == "__main__":
 
             st.write(f"Accuracy Results after adding left and right boundary points: **{0.5}**")
 
-            st.write(f"Difference/Feature importance of the selected interval: **{abs(results - 0.5):.4f}**")
+            st.write(f"Difference/Feature importance of the selected interval: **{abs(results_1 - 0.5):.4f}**")
 
     with feature_importance_model_2:
         st.subheader(option_model_selection_2)
 
         sorted_idx = np.argsort(feature_importance_2)[::-1][:top_n]  # Korrekte Sortierung
         sorted_importance = feature_importance_2[sorted_idx]
-        sorted_features = df_test.columns[sorted_idx]
+        sorted_features = df_test_2.columns[sorted_idx]
 
-        top_features_df = pd.DataFrame({
+        top_features_df_2 = pd.DataFrame({
             'Feature': sorted_features,
             'Importance': sorted_importance
         })
 
         fig = px.bar(
-            top_features_df,
+            top_features_df_2,
             x='Feature',
             y='Importance',
             orientation='v',
@@ -596,38 +627,38 @@ if __name__ == "__main__":
 
         selected_feature = st.selectbox(
             "Wähle ein Feature aus:",
-            options=top_features_df['Feature'],
+            options=top_features_df_2['Feature'],
             key="feature_selection_2"
         )
 
         if selected_feature:
             num_intervals = option_number_intervals
-            intervals, interval_info = partition_dataframe(X_test, num_intervals)
-            selected_interval = find_interval(selected_feature, interval_info)
+            intervals_2, interval_info_2 = partition_dataframe(X_test_2, num_intervals)
+            selected_interval_2 = find_interval(selected_feature, interval_info_2)
 
             # Ergebnisse anzeigen
-            st.write(f"Selected interval: **{selected_interval['interval_number']}** (of {num_intervals})")
-            st.write(f"Start value: **{selected_interval['start']}**")
-            st.write(f"End value: **{selected_interval['end']}**")
-            st.write(f"Number of features in interval: **{selected_interval['n_columns']}**")
+            st.write(f"Selected interval: **{selected_interval_2['interval_number']}** (of {num_intervals})")
+            st.write(f"Start value: **{selected_interval_2['start']}**")
+            st.write(f"End value: **{selected_interval_2['end']}**")
+            st.write(f"Number of features in interval: **{selected_interval_2['n_columns']}**")
 
-        selected_interesting_interval = st.selectbox(
+        selected_interesting_interval_2 = st.selectbox(
             "Select an interval:",
             options=[f"{i}" for i in range(1, num_intervals + 1)],
             key="interval_selection_2"
         )
 
-        if selected_interesting_interval:
-            results = calculate_interval_error_rf(
-                df_test,
-                int(selected_interesting_interval) - 1,
-                y_test,
-                intervals,
-                interval_info,
+        if selected_interesting_interval_2:
+            results_2 = calculate_interval_error_rf(
+                df_test_2,
+                int(selected_interesting_interval_2) - 1,
+                y_test_2,
+                intervals_2,
+                interval_info_2,
                 rf_model_2
             )
 
-            st.write(f"Accuracy Results before adding left and right boundary points: **{results:.4f}**")
+            st.write(f"Accuracy Results before adding left and right boundary points: **{results_2:.4f}**")
 
             #combined_points = combine_interval_points(
                 #df_test,
@@ -650,4 +681,4 @@ if __name__ == "__main__":
 
             st.write(f"Accuracy Results after adding left and right boundary points: **{0.4}**")
 
-            st.write(f"Difference/Feature importance of the selected interval: **{abs(results - 0.4):.4f}**")
+            st.write(f"Difference/Feature importance of the selected interval: **{abs(results_2 - 0.4):.4f}**")
